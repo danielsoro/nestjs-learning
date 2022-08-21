ff3a5ff2f67f9e937245308d3da797d4fa294cd4
Daniel Cunha <danielsoro@gmail.com>
2022-08-21 12:03:13 -0300

    feat: improve config module add schema validations and loading from env

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,20 +4,33 @@ import { AppService } from './learning/app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
-    CoffeesModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'pass123',
-      database: 'postgres',
-      autoLoadEntities: true,
-      synchronize: true,
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        DATABASE_HOST: Joi.required().default('localhost'),
+        DATABASE_NAME: Joi.required().default('postgres'),
+        DATABASE_USER: Joi.required().default('postgres'),
+        DATABASE_PASSWORD: Joi.required(),
+        DATABASE_PORT: Joi.required().default(5432),
+      }),
     }),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.DATABASE_HOST,
+        port: +process.env.DATABASE_PORT,
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+    CoffeesModule,
     CoffeeRatingModule,
   ],
   controllers: [AppController],
