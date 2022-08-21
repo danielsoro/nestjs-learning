3014046e8f52074db7f0a3f4b975e40701f6dc36
Daniel Cunha <danielsoro@gmail.com>
2022-08-21 20:04:24 -0300

    fix: fixing coffees.controller.spec

--- src/coffees/coffees.controller.spec.ts
+++ src/coffees/coffees.controller.spec.ts
@@ -8,6 +8,7 @@ import { Flavor } from './entities/flavors.entity';
 
 describe('CoffeesController', () => {
   let controller: CoffeesController;
+  let service: CoffeesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -27,9 +28,28 @@ describe('CoffeesController', () => {
     }).compile();
 
     controller = module.get<CoffeesController>(CoffeesController);
+    service = module.get<CoffeesService>(CoffeesService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('findOne', () => {
+    const coffeeId = 1;
+
+    describe('when coffee with ID exists', () => {
+      it('should return the coffee object', async () => {
+        const expectedCoffee = {};
+
+        jest
+          .spyOn(service, 'findOne')
+          .mockReturnValue(Promise.resolve(expectedCoffee as Coffee));
+        const coffee = await service.findOne(coffeeId);
+
+        expect(coffee).toBeDefined();
+        expect(coffee).toEqual(expectedCoffee);
+      });
+    });
   });
 });
