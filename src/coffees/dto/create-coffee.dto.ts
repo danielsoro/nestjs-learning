cd6a62d1326ac3ebe2188a947498266953473f21
Daniel Cunha <danielsoro@gmail.com>
2022-08-21 07:25:25 -0300

    Fix models and remove warnings from the code

--- src/coffees/dto/create-coffee.dto.ts
+++ src/coffees/dto/create-coffee.dto.ts
@@ -1,8 +1,12 @@
-import { IsString } from 'class-validator';
+import { IsOptional, IsString } from 'class-validator';
 
 export class CreateCoffeeDto {
   @IsString()
-  readonly name: string;
+  readonly title: string;
+
+  @IsString()
+  @IsOptional()
+  readonly descriptiom: string;
 
   @IsString()
   readonly brand: string;
